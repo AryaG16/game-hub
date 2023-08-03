8e79961a1197defabb992de0d2721f7d53a39776
Aryan Bhavsar <aryanbhavsar161202@gmail.com>
2023-08-03 15:09:08 +0530

    Displaying Genres

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image borderRadius={28} src={logo} boxSize="60px" />
       <ColorModeSwitch />
     </HStack>
   );
